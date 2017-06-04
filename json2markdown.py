d34c8cb1175228c244ecac9f402bda9e5b668e4a
Jeff-HOU <1085363891@qq.com>
2017-06-04 22:38:50 +0800

    Update json2markdown.py

--- json2markdown.py
+++ json2markdown.py
@@ -19,83 +19,63 @@ curl_content = '{"title":"'+"title"+'""}'
 curl_cmd = 'curl -X PUT -d '+ curl_content + 'https://hotmelon-655ec.firebaseio.com/ppt_test/'+slideCount+'/'+elementCount+'.json'
 '''
 with urllib.request.urlopen(link) as url:
+    # load json, set basic vars
     slideCount = 1
     elementCount = 0
-    element = []
-    #data_element = json.dumps({})
-    keyword = []
-
     data = json.loads(url.read().decode())
     f = open(file, 'w+')
     f.close()
     f = open(file, 'a')
     f.write('---\n\n')
-    sd = data[name] # maybe this is also an input?
+    sd = data[name]
     bgcolor = data["style"]["backgroundColor"]
     color = data["style"]["color"]
-    colorPic = "https://34.208.199.223/color/"
+    colorPic = "https://hotmelon.tech/color/"
+    
+    # clear the corresponding PPT folder, set dome basic elements and bgColor
     firebase.put('/', tmp_ppt, data = '')
     firebase.put(ppt+'/'+str(slideCount), "bg", data = bgcolor)
     firebase.put(ppt+'/'+str(slideCount), "color", data = color)
     slideCount += 1
-
     f.write("![]("+colorPic+bgcolor+".png"+"){.background}\n\n")
-    #element.append({str(elementCount)+"bg" : bgcolor})
 
-    
-    #elementCount += 1
+    # set title
     f.write("# "+'<span style="color: '+color+'">'+sd["title"]+'</span>'+"\n\n")
-    #element.append({str(elementCount)+"title" : sd["title"]})
     firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"title", data = sd["title"])
     elementCount += 1
-
-    '''
-    put_content = '{"title":"'+sd["title"]+'""}'
-    print (put_content)
-    put_cmd = ['curl', '-X', 'PUT', '-d', put_content, 'https://hotmelon-655ec.firebaseio.com/ppt_test/0.json']
-    try:
-        output = subprocess.check_output(put_cmd, stderr=subprocess.STDOUT)
-        retcode = 0
-    except subprocess.CalledProcessError, e:
-        retcode = e.returncode
-        output = e.output
-    elementCount += 1
     
-    fh = os.popen(curl_cmd, 'r')
-    data = fh.read()
-    fh.close()
-    '''
-
+    # set subtitle
     f.write("## "+'<span style="color: '+color+'">'+sd["subtitle"]+'</span>'+"\n\n")
-    #element.append({str(elementCount)+"subtitle": sd["subtitle"]})
     firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"subtitle", data = sd["subtitle"])
     elementCount += 1
+    
+    # set authors
     #atr = ', '.join(sd["authors"])
     #f.write('<span style="color: grey">Authors: '+atr+"</span>\n\n")
+    
+    # set notes
     f.write('<!--'+'\n')
     f.write(sd["abstract"]+"\n")
     f.write('-->'+'\n\n')
+    
+    # create new page, set bgColor
     f.write('---\n\n')
-    #print (element)
-    #json_element = json.dumps(element)
-    #firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"bg", data = {{json_element}})
     slideCount += 1
     elementCount = 0
     f.write("![]("+colorPic+bgcolor+".png"+"){.background}\n\n")
-    #firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"bg", data = bgcolor)
-    #elementCount += 1
+
     cptr = sd["chapters"]
     for i, _ in enumerate(cptr):
         scptr = cptr[i]
         f.write("# "+'<span style="color: '+color+'">'+scptr["title"]+'</span>'+"{.big}"+"\n\n")
         firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"title", data = scptr["title"])
         elementCount += 1
+        
         f.write("---\n\n")
         slideCount += 1
         elementCount = 0
         f.write("![]("+colorPic+bgcolor+".png"+"){.background}\n\n")
-        #firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"bg", data = bgcolor)
-        #elementCount += 1
+
         sec = scptr["sections"]
         for j, _ in enumerate(sec):
             ssec = sec[j]
@@ -104,19 +84,23 @@ with urllib.request.urlopen(link) as url:
                 if not ssec["figures"] == "":
                     noFignoKwdId = 0
                     for k, val in enumerate(ssec["figures"]):
+                        # set title
                         f.write("# "+'<span style="color: '+color+'">'+ssec["title"]+'</span>'+"\n\n")
                         firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"title", data = ssec["title"])
                         elementCount += 1
+                        
+                        # convert and put figures
                         fig = ssec["figures"][k]
                         f.write("![]("+fig+")"+"\n\n")
                         firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"figure", data = fig)
                         elementCount += 1
+                        
+                        # create new page, set bgColor
                         f.write("---\n\n")
                         slideCount += 1
                         elementCount = 0
                         f.write("![]("+colorPic+bgcolor+".png"+"){.background}\n\n")
-                        #firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"bg", data = bgcolor)
-                        #elementCount += 1
+
             except:
                 print("", end='')
             try:
@@ -129,8 +113,8 @@ with urllib.request.urlopen(link) as url:
                     for k, val in enumerate(ssec["keywords"]):
                         f.write("* "+'<span style="color: '+color+'">'+val+'</span>'+"\n\n")
                         keyword.append(val)
-                        #firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"keyword", data = val)
-                        #elementCount += 1
+                        
+                        # if index of keywords and reach 10n, create a new page and continue.
                         if (k % 10 == 0 and k > 0):
                             f.write("---\n\n")
                             firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"keyword", data = ', '.join(keyword))
@@ -138,24 +122,23 @@ with urllib.request.urlopen(link) as url:
                             slideCount += 1
                             elementCount = 0
                             f.write("![]("+colorPic+bgcolor+".png"+"){.background}\n\n")
-                            #firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"bg", data = bgcolor)
-                            #elementCount += 1
+                            
+                            # if the last index is a 10n, do not create a new title for it.
                             if not k == len(ssec["keywords"]) - 1:
                                 f.write("# "+'<span style="color: '+color+'">'+ssec["title"]+'</span>'+"\n\n")
                                 firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"title", data = ssec["title"])
                                 elementCount += 1
-                        tmp = k
+                        tmp = k # used to keep track of k.
                     if not tmp % 10 == 0:
                         f.write("---\n\n")
                         slideCount += 1
                         elementCount = 0
                         f.write("![]("+colorPic+bgcolor+".png"+"){.background}\n\n")
-                        #firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"bg", data = bgcolor)
-                        #elementCount += 1
                         
             except:
                 print("", end='')
             if noFignoKwdId:
+                # if there is no figure ot keyword, display only section title
                 f.write("# "+'<span style="color: '+color+'">'+ssec["title"]+'</span>'+"{.big}\n\n")
                 firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"title", data = ssec["title"])
                 elementCount += 1
@@ -163,6 +146,5 @@ with urllib.request.urlopen(link) as url:
                 slideCount += 1
                 elementCount = 0
                 f.write("![]("+colorPic+bgcolor+".png"+"){.background}\n\n")
-                #firebase.put(ppt+'/'+str(slideCount),str(elementCount)+"bg", data = bgcolor)
-                #elementCount += 1
+
     f.close()
